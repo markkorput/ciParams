88c6df2cde248f867629f9fd6b11b9b553c81abe
Mark van de Korput <mark.korput@gmail.com>
2017-05-27 14:37:38 +0200

    ParameterInterface get and set functionality

--- src/ciParams/ParameterInterface.h
+++ src/ciParams/ParameterInterface.h
@@ -3,3 +3,21 @@
 using namespace ci;
 using namespace ci::app;
 using namespace std;
+
+namespace params {
+    template<class T>
+    class ParameterInterface {
+    public:
+        ParameterInterface() : pParam(NULL){}
+        ParameterInterface(T& param) : pParam(&param){}
+        
+        const T& get() const { return *pParam; }
+        void set(const T& newValue){ *pParam = newValue; }
+
+        const T* getParameter() const { return pParam; }
+        void setParameter(T& param){ pParam = &param; }
+    private:
+        T* pParam;
+    };
+}
+
--- tests/main.cpp
+++ tests/main.cpp
@@ -4,13 +4,25 @@
 #include "ciParams.h"
 
 using namespace ci;
-//using namespace ci::app;
+using namespace params;
 //using namespace std;
 
-TEST_CASE("ciParams", "")
+TEST_CASE("params::ParameterInterface", "")
 {
-    SECTION("Default Values")
+    SECTION("get and set")
     {
-        REQUIRE(1==1);
+        string p1 = "value1";
+        ParameterInterface<string> pi(p1);
+        REQUIRE(pi.getParameter() == &p1);
+        REQUIRE(pi.get() == "value1");
+        
+        string p2 = "value_2";
+        pi.setParameter(p2);
+        REQUIRE(pi.getParameter() == &p2);
+        REQUIRE(pi.get() == "value_2");
+        
+        pi.set("value#3");
+        REQUIRE(pi.get() == "value#3");
+        REQUIRE(p2 == "value#3");
     }
 }
