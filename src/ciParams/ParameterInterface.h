e14a3ea27b11f013ae6d1f9eb021240e4700aff6
Mark van de Korput <mark.korput@gmail.com>
2017-05-27 14:25:09 +0200

    unit tests project

---
+++ src/ciParams/ParameterInterface.h
@@ -0,0 +1,5 @@
+#include "cinder/app/App.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
---
+++ tests/main.cpp
@@ -0,0 +1,16 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "ciParams.h"
+
+using namespace ci;
+//using namespace ci::app;
+//using namespace std;
+
+TEST_CASE("ciParams", "")
+{
+    SECTION("Default Values")
+    {
+        REQUIRE(1==1);
+    }
+}
